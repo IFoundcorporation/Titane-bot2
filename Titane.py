6cbcc529f8b84c186059f52d96286df3b32a65f9
IFoundcorporation <60831701+IFoundcorporation@users.noreply.github.com>
2020-11-29 19:45:05 -0500

    third upload

--- Titane.py
+++ Titane.py
@@ -6,7 +6,8 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="!")
 NDEL = "no title"
 
-scores = []
+scoresDict = {}
+scoresList = []
 
 
 @bot.event
@@ -17,7 +18,7 @@ async def on_ready():
 @bot.command()
 async def new(ctx):
     global NDEL
-    await ctx.send("Quel est le nom de l'examin?")
+    await ctx.send("What's the name of the exam?")
 
     def check(message):
         return message.author == ctx.message.author and ctx.message.channel == message.channel
@@ -26,21 +27,21 @@ async def new(ctx):
     print(NDE.content)
     NDEL = NDE.content
     await look(ctx)
-    await rebirth()
+    await rebirth(ctx)
 
 
 @bot.command()
 async def look(ctx):
     try:
      embed = discord.Embed(title=NDEL, description="classement")
-     embed.add_field(name="top1", value=scores[0], inline=False)
-     embed.add_field(name="top2", value=scores[1], inline=False)
-     embed.add_field(name="top3", value=scores[2], inline=False)
-     embed.add_field(name="top4", value=scores[3], inline=False)
-     embed.add_field(name="top5", value=scores[4], inline=False)
+     embed.add_field(name="top1", value=scoresList[0], inline=False)
+     embed.add_field(name="top2", value=scoresList[1], inline=False)
+     embed.add_field(name="top3", value=scoresList[2], inline=False)
+     #embed.add_field(name= "top4", value= scoresList[3], inline=False)
+     #embed.add_field(name= "top5", value= scoresList[4], inline=False)
      await ctx.send(embed=embed)
     except:
-     await ctx.send("Add at least 5 grades to see the classment")
+     await ctx.send("Add at least 3 grades to see the classment")
 
 
 #changer les value pour des variables
@@ -48,22 +49,33 @@ async def look(ctx):
 
 @bot.command()
 async def addscore(ctx):
+    tusername = ctx.author
     await ctx.send("what is your grade")
 
     def check(message):
         return message.author == ctx.message.author and ctx.message.channel == message.channel
 
     NewScore = await bot.wait_for("message", timeout=10, check=check)
+    print(tusername)
     print(NewScore.content)
-    scores.append(NewScore.content)
-    scores.sort(reverse=True)
-    print(scores)
+    #modifier
+    scoresDict[tusername.name] = NewScore.content
+    print(scoresDict)
+    await sortlist()
+    print(scoresList)
     return NewScore
 
 
 @bot.command()
-async def rebirth():
-    scores.clear()
+async def rebirth(ctx):
+    scoresDict.clear()
+    scoresList.clear()
+
+
+@bot.command()
+async def sortlist():
+    global scoresList
+    scoresList = sorted(scoresDict.items(), key=lambda t: t[1], reverse=True)
 
 
 bot.run("NzgxMjAxNjk4ODMxNDY2NTE3.X76Mxw.OoVlZ2STGqanIormm-7nEHIPcHY")
